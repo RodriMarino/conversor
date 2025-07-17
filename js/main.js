00b211fd88a9dd58fda475b467a806a1f6ad903a
Rodrigo Marino <rodromarino1993@gmail.com>
2025-07-17 12:11:20 +0200

    segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,33 +1,74 @@
-const tasaDolar = 0.00084
-const tasaEuro = 0.00072
-
-let monedasDisponibles = ["usd", "eur"]
-
-let comenzar = confirm("¿Desea convertir de pesos a otra moneda?")
-let tipoDeConversion
-let resultado
-
-if (comenzar) {
-  alert("Monedas disponibles: " + monedasDisponibles.join("-")
-) 
-
-  tipoDeConversion = prompt("Ingrese el tipo de conversión (usd o eur):")
-  let pesos = parseFloat(prompt("Ingrese la cantidad de pesos a convertir:"))
-
-  if (tipoDeConversion === "eur") {
-    resultado = pesos * tasaEuro;
-    alert("El resultado es: " + resultado + " euros.")
-  } 
-  
-  else if (tipoDeConversion === "usd") {
-    resultado = pesos * tasaDolar;
-    alert("El resultado es: " + resultado + " dólares.")
-  }
-
-else 
-  alert("Conversión cancelada.")
+//Tasas, coeficiente.
+const tasas = {
+    usd: 0.00084,
+    eur: 0.00072,
+    lib: 0.00063
 }
 
-continuar = confirm("¿Querés hacer otra conversión?")
+//Tipos de tasas
+const tipoDeTasas = [
+    {id: 1,
+    moneda: "Dolar Estadounidense",
+    simbolo: "U$D"
+    },
+    {
+    id:2,
+    moneda: "Euro",
+    simbolo:"€"
+    },
+    {
+    id: 3,
+    moneda: "Libra Esterlina",
+    simbolo: "£"
+    }
+]
 
-alert("Gracias por usar el conversor. Hasta la próxima!")
+
+//DOM
+let input = document.getElementById("input")
+let calcular = document.getElementById("calcular")
+let selectMoneda = document.getElementById("moneda")
+let banner = document.getElementById("banner")
+
+//Recorrer el array.
+function renderTasas(tipoDeTasas) {
+    const lista =document.createElement("ul")
+    tipoDeTasas.forEach(tasa => {
+        const divisas = document.createElement("li")
+        divisas.innerHTML = `<h4>${tasa.moneda} ${tasa.simbolo}.</h4>`
+        lista.appendChild(divisas)
+        banner.appendChild(lista)
+    })
+}
+renderTasas(tipoDeTasas)
+
+
+//Onclick
+calcular.onclick = () => {
+    let monedaSeleccionada = selectMoneda.value
+    let resultado
+    let print = document.createElement("h5")
+
+    switch (monedaSeleccionada) {
+        case "usd":
+            resultado = input.value * tasas.usd
+            print.innerHTML = `$${input.value} son USD ${resultado}`
+            break
+        case "eur":
+            resultado = input.value * tasas.eur;
+            print.innerHTML = `$${input.value} son EUR ${resultado}`
+            break
+        case "lib":
+            resultado = input.value * tasas.lib
+            print.innerHTML = `$${input.value} son GBP ${resultado}`
+            break
+        default:
+            print.innerHTML = "Moneda no válida"
+    }
+    document.body.append(print)
+
+//Almacenamiento
+    localStorage.setItem("conversion", print.innerHTML)
+}
+
+
